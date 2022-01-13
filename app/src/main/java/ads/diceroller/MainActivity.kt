1731655fb71d0d49673a7bd6de1359f1d6f24ede
Pratyush Kumar <pratyush20454@iiitd.ac.in>
2022-01-13 20:49:07 +0530

    Added dice functionality
    Signed-off-by: Pratyush Kumar <pratyush20454@iiitd.ac.in>

--- app/src/main/java/ads/diceroller/MainActivity.kt
+++ app/src/main/java/ads/diceroller/MainActivity.kt
@@ -1,11 +1,35 @@
 package ads.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the roll button and call rollDice() when clicked.
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+
+    }
+
+    // Add a toast and change the textview to dice roll
+    private fun rollDice() {
+        Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+        val diceRoll = Dice(6).roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
