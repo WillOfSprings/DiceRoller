3c56930c0a70f8147137ff593400bd3b358afa23
Pratyush Kumar <pratyush20454@iiitd.ac.in>
2022-01-14 20:29:23 +0530

    Testing Logging
    Signed-off-by: Pratyush Kumar <pratyush20454@iiitd.ac.in>

--- app/src/main/java/ads/diceroller/MainActivity.kt
+++ app/src/main/java/ads/diceroller/MainActivity.kt
@@ -1,18 +1,23 @@
 package ads.diceroller
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        logging()
 
         // Find the roll button and call rollDice() when clicked.
         val rollButton: Button = findViewById(R.id.button)
+        rollDice()
         rollButton.setOnClickListener { rollDice() }
 
     }
@@ -20,9 +25,28 @@ class MainActivity : AppCompatActivity() {
     // Add a toast and change the textview to dice roll
     private fun rollDice() {
         Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-        val diceRoll = Dice(6).roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceRoll: Int = Dice(6).roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+    }
+
+    private fun logging() {
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
 
 }
